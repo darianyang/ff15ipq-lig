34455116f2c5cc17c38cddc1b2e2bd9b038a4929
Darian T. Yang <dty7@pitt.edu>
2022-03-22 16:14:56 -0400

    make clean branch

--- charges/gaff_00/GenConformers/vmd.tcl
+++
@@ -1,8 +0,0 @@
-for {set a 1} {$a < 21} {incr a} { 
- mol new Conf${a}/Conf${a}.pdb 
- mol selection {not water} 
- mol representation Licorice 0.2 50 50 
- } 
- axes location off 
- color Display Background white 
- display antialias on
